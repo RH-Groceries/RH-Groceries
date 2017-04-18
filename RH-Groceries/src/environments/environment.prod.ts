1723e65aad128cfc80cf886f1e98131c816e83de
JJGayso <gaysojj@rose-hulman.edu>
2017-04-18 15:30:52 -0400

    Added Firebase Config Environments

---
+++ RH-Groceries/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmRs3RgHxNoDHE8lCcuh2daShCqOuRcJY",
+    authDomain: "rh-groceries.firebaseapp.com",
+    databaseURL: "https://rh-groceries.firebaseio.com",
+    projectId: "rh-groceries",
+    storageBucket: "rh-groceries.appspot.com",
+    messagingSenderId: "65234419210"
+  }
+};
---
+++ RH-Groceries/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmRs3RgHxNoDHE8lCcuh2daShCqOuRcJY",
+    authDomain: "rh-groceries.firebaseapp.com",
+    databaseURL: "https://rh-groceries.firebaseio.com",
+    projectId: "rh-groceries",
+    storageBucket: "rh-groceries.appspot.com",
+    messagingSenderId: "65234419210"
+  }
+};
