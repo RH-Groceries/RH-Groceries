01844506afc3968f5959f9f18315217ed42febc2
JJGayso <gaysojj@rose-hulman.edu>
2017-04-23 23:23:55 -0400

    More States

--- RH-Groceries/src/models/shopping-list.ts
+++ RH-Groceries/src/models/shopping-list.ts
@@ -11,7 +11,10 @@ export class ShoppingList {
     // TODO: Need to establish what statuses there are...
     // 0: Inactive
     // 1: Active
-    // 2: Complete
+    // 2: AwaitingBuyerConfirmation
+    // 3: Shopping
+    // 4: AwaitingBuyerApproval
+    // 5: Complete
 
     get nameForCard(): string {
         console.log("Something here");
