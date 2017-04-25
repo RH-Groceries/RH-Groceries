ea13a49c486a784748c8214102444f709f15ba41
JJGayso <gaysojj@rose-hulman.edu>
2017-04-24 20:30:14 -0400

    Added User Model

---
+++ RH-Groceries/src/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    name: string = "";
+    address: string =  "";
+    phone: string = "";
+    image: string = "";
+    buyerRating: number = -1.0;
+    shopperRating: number = -1.0;
+    
+}
