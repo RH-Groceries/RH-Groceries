2b51307b5f1574367402a4b703823f97ac3f5ecb
Aaron Mercier <mercieal@rose-hulman.edu>
2017-05-09 00:02:01 -0400

    Pictures!

--- RH-Groceries/src/pages/profile-setup/profile-setup.ts
+++ RH-Groceries/src/pages/profile-setup/profile-setup.ts
@@ -3,7 +3,6 @@ import { AuthService } from './../../providers/auth-service';
 import { TabPage } from './../tab-page/tab-page';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ImagePicker } from '@ionic-native/image-picker';
 import * as firebase from 'firebase';
 import { User } from "../../models/user";
 
@@ -16,10 +15,12 @@ import { User } from "../../models/user";
 export class ProfileSetup {
 
   public user: User;
-  public imageUrl: string = "";
+  public loadingImage: boolean = false;
+  public tempPhotoUrl: string;
+  private photo: File;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private imagePicker: ImagePicker, private authService: AuthService, private http: Http) {
-        this.user = new User();
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService, private http: Http) {
+    this.user = new User();
   }
 
   ionViewDidLoad() {
@@ -38,31 +39,46 @@ export class ProfileSetup {
     });
 
     this.user.name = this.authService.rfUser.name;
-    firebase.database().ref().child('users').child(this.authService.authState.uid).set(this.user, (err) => {
-      if (err) {
-        //do something
-      }
-      else {
-
+    if (this.photo && this.tempPhotoUrl) {
+      this.loadingImage = true;
+      const metadata = { "content-type": this.photo.type }
+      const storageRef: firebase.storage.Reference = firebase.storage().ref().child("photos").child(this.authService.authState.uid);
+      const uploadTask: firebase.storage.UploadTask = storageRef.put(this.photo, metadata);
+      uploadTask.then((uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
+        const downloadUrl = uploadSnapshot.downloadURL;
+        this.user.image = downloadUrl;
+        firebase.database().ref().child('users').child(this.authService.authState.uid).set(this.user, (err) => {
+          this.loadingImage = false;
+          this.navCtrl.setRoot(TabPage);
+        });
+      });
+    }
+    else {
+      firebase.database().ref().child('users').child(this.authService.authState.uid).set(this.user, (err) => {
         this.navCtrl.setRoot(TabPage);
-      }
-    });
-  }
-
-  pickImage() {
-    console.log("Opening image picker");
-    let options = {
-      maximumImagesCount: 1,
-      width: 500,
-      height: 500,
-      quality: 50
+      });
     }
 
-    this.imagePicker.getPictures(options).then((file_uris) => {
-      this.imageUrl = file_uris[0];
-      //do something
-    }, (err) => {
-      console.log('Picker failed (are you running in a mobile simulator?)');
-    });
+  }
+
+  photoSelected(event: any) {
+    this.photo = event.target.files[0];
+    if (this.photo) {
+      this.loadingImage = true;
+      const metadata = { "content-type": this.photo.type }
+      const storageRef: firebase.storage.Reference = firebase.storage().ref().child("photos").child(this.authService.authState.uid + "temp");
+      const uploadTask: firebase.storage.UploadTask = storageRef.put(this.photo, metadata);
+      uploadTask.then((uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
+        const downloadUrl = uploadSnapshot.downloadURL;
+        this.tempPhotoUrl = downloadUrl;
+        this.loadingImage = false;
+      });
+    }
+
+  }
+
+  removePhoto() {
+    this.tempPhotoUrl = "";
+    this.photo = null;
   }
 }
